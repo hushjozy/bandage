6eb6d00eb5d4d90643035fe7d47c2469a10a4ca4
Joseph <hushjoezzybad@gmail.com>
2024-01-18 00:02:07 +0100

    summing

--- src/component/ItemModal.tsx
+++ src/component/ItemModal.tsx
@@ -30,15 +30,15 @@ const ItemModal: React.FC<ItemModalProps> = ({ isOpen, onClose, items, isCart })
       overlayClassName="fixed inset-0 bg-black flex justify-center align-center centralModCont"
       >
           
-      <h2 className="text-2xl mb-4">{items instanceof Array && items.length > 0 ? 'Items' : 'No items to display'}</h2>
+      <h2 className="text-3xl mb-4 font-bold text-uppercase text-center ">{items instanceof Array && items.length > 0 ? 'Items' : 'No items to display'}</h2>
       {items instanceof Array && items.length > 0 && (
         <table className="w-full border-collapse table-auto">
           <thead>
             <tr className="bg-gray-200">
               <th className="py-2 px-4 border w-1/4 balancetab">Title</th>
-              {items instanceof Array && items[0].hasOwnProperty('quantity') && (
+              {/* {items instanceof Array && items[0].hasOwnProperty('quantity') && (
                 <th className="py-2 px-4 border w-1/4 balancetab">Quantity</th>
-              )}
+              )} */}
               {items instanceof Array && items[0].hasOwnProperty('price') && <th className="py-2 px-4 border">Price</th>}
               <th className="py-2 px-4 border w-16 h-16 w-1/4 balancetab">Thumbnail</th>
               <th className="py-2 px-4 border w-16 h-16 w-1/4 balancetab">Remove</th>
@@ -51,7 +51,7 @@ return(
                 
               <tr key={item.id} className="border">
                 <td className="py-2 px-4 border w-1/4 balancetab">{item.title}</td>
-                {item.hasOwnProperty('quantity') && <td className="py-2 px-4 border w-1/4 balancetab">{(item as CartItem).quantity}</td>}
+                {/* {item.hasOwnProperty('quantity') && <td className="py-2 px-4 border w-1/4 balancetab">{(item as CartItem).quantity}</td>} */}
                 {item.hasOwnProperty('price') && <td className="py-2 px-4 border w-1/4 balancetab">${discountedPrice}</td>}
                 <td className="py-2 px-4 border w-16 h-16 w-1/4 balancetab">
                   <img src={item.thumbnail} alt={item.title} className="w-16 h-16 object-cover" />
@@ -79,7 +79,7 @@ onClick={() => handleRemoveItem(item.id)}        className=" px-10  bg-sky-500 o
       )}
       <button
         onClick={()=>onClose()}
-        className="absolute bottom-0 right-[31px] px-10  bg-sky-500 opacity-100 text-white py-2  rounded-md hover:bg-sky-700 focus:outline-none w-auto bg-sky-500"   style={{bottom:'12px',right:'31px'}} >
+        className="absolute bottom-0 right-[31px] px-10  bg-sky-500 opacity-100 text-white py-2  rounded-md hover:bg-sky-700 focus:outline-none w-auto bg-sky-500"   style={{bottom:'22px',right:'31px'}} >
         Close Modal
       </button>
     </Modal>
